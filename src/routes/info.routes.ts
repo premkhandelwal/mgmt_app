09be5681cbd9a5333b32ce370ea82f9a8aed4b42
premkhandelwal <prem.dkhandelwal@gmail.com>
2025-08-05 23:13:58 +0530

    Changed post to delete

--- src/routes/info.routes.ts
+++ src/routes/info.routes.ts
@@ -5,9 +5,9 @@ const router: Router = Router()
 
 // router.post('/addNotificationToken', info.addNotificationToken)
 
-router.post('/addPayment', info.addPayment)
-router.delete('/deletePayment', info.deletePayment)
 
+router.post('/addPayment', info.addPayment)
 router.get('/fetchPayments', info.fetchPayments)
+router.delete('/deletePayment', info.deletePayment)
 
 module.exports = router;
